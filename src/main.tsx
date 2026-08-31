40ed056f6901dcffc923c108db2a258ad020ae09
Akhand Singh <akhandx2@gmail.com>
2026-08-31 14:24:28 +0530

    Wrap KrishiAI with Algorand Testnet wallet provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { WalletProvider } from '@txnlab/use-wallet-react';
 import App from './App.tsx';
+import { algorandWalletManager } from './services/algorandWallet';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <WalletProvider manager={algorandWalletManager}>
+      <App />
+    </WalletProvider>
   </StrictMode>,
 );
